afd45ec8de48d5a8b0c6dcdc440cd5786e326283
ENBT <ian@enbt.net>
2017-04-09 16:30:17 -0500

    forcepush
    yahyahyahyah

---
+++ Assets/IansScripts/ForcePush.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForcePush : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject player;
+    public float intensity;
+    Collider[] pushThese;
+    Collider[] pullThese;
+    public LayerMask layers;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("the Spacebar still works");
+            forcePush();
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    void forcePush()
+    {
+        pushThese = Physics.OverlapBox(new Vector3(transform.position.x, transform.position.y, transform.position.z),
+            new Vector3(4.5f, 2, 2), Quaternion.identity, layers);
+        if (pushThese.Length > 0)
+        {
+            foreach (Collider a in pushThese)
+            {
+                Debug.Log("pushing this guy: " + a);
+                a.GetComponent<Rigidbody>().AddForce(-(player.transform.position - a.transform.position) * intensity);
+            }
+        }
+    }
+}
