07493055392b5f995fd8fe8a68b7c8f13927541f
ENBT <ian@enbt.net>
2017-04-09 14:40:01 -0500

    small stuff
    nothing really that useful

---
+++ Assets/IansScripts/ForcePush.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForcePush : MonoBehaviour
+{
+
+    [SerializeField]
+    float power;
+    Vector3 centerOfBox;
+    [SerializeField]
+    GameObject player;
+
+
+    void Start()
+    {
+        centerOfBox = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+        if (Input.GetKeyDown(KeyCode.Space)) //temporary way to activate the force push
+
+        {
+            Debug.Log("space is pressed");
+            if (Physics.BoxCast(centerOfBox, new Vector3(1, 5, 2), Vector3.forward, out hit, new Quaternion(), 10))
+            {
+                Debug.Log("what is working?");
+            }
+
+        }
+
+    }
+}
