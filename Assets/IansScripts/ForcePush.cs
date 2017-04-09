2b4c36ea794e55ecde918feeb02ad4e26c7249f5
ENBT <ian@enbt.net>
2017-04-09 17:25:40 -0500

    forcepull
    force pull and force push with a test scene to test the ranges

--- Assets/IansScripts/ForcePush.cs
+++ Assets/IansScripts/ForcePush.cs
@@ -25,6 +25,11 @@ public class ForcePush : MonoBehaviour
             Debug.Log("the Spacebar still works");
             forcePush();
         }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            Debug.Log("the I key still works");
+            forcePull();
+        }
 
     }
 
@@ -35,14 +40,27 @@ public class ForcePush : MonoBehaviour
 
     void forcePush()
     {
-        pushThese = Physics.OverlapBox(new Vector3(transform.position.x, transform.position.y, transform.position.z),
-            new Vector3(4.5f, 2, 2), Quaternion.identity, layers);
+        pushThese = Physics.OverlapBox(new Vector3(transform.position.x + 5, transform.position.y, transform.position.z),
+            new Vector3(10f, 2, 2), Quaternion.identity, layers);
         if (pushThese.Length > 0)
         {
             foreach (Collider a in pushThese)
             {
                 Debug.Log("pushing this guy: " + a);
-                a.GetComponent<Rigidbody>().AddForce(-(player.transform.position - a.transform.position) * intensity);
+                a.GetComponent<Rigidbody>().AddForce(-(player.transform.position - a.transform.position) * intensity*2);
+            }
+        }
+    }
+    void forcePull()
+    {
+        pullThese = Physics.OverlapBox(new Vector3(transform.position.x + 10, transform.position.y, transform.position.z),
+            new Vector3(4.5f, 2, 2), Quaternion.identity, layers);
+        if (pullThese.Length > 0)
+        {
+            foreach (Collider a in pullThese)
+            {
+                Debug.Log("pulling this guy: " + a);
+                a.GetComponent<Rigidbody>().AddForce((player.transform.position - a.transform.position) * intensity / 2);
             }
         }
     }
