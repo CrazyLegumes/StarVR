6c2a9537404e4d0f8fa1a1c9120a093dcd67b40f
ENBT <ian@enbt.net>
2017-04-09 19:27:37 -0500

    put the lightsaber in the right stop

--- Assets/IansScripts/ForcePush.cs
+++ Assets/IansScripts/ForcePush.cs
@@ -10,6 +10,7 @@ public class ForcePush : MonoBehaviour
     Collider[] pushThese;
     Collider[] pullThese;
     public LayerMask layers;
+    Vector3 force;
 
     void Start()
     {
@@ -39,28 +40,24 @@ public class ForcePush : MonoBehaviour
 
     void forcePush()
     {
-        pushThese = Physics.OverlapBox(new Vector3(transform.position.x + 5, transform.position.y, transform.position.z),
+        pushThese = Physics.OverlapBox(new Vector3(transform.position.x + 10, transform.position.y, transform.position.z),
             new Vector3(10f, 2, 2), Quaternion.identity, layers);
-        if (pushThese.Length > 0)
+        foreach (Collider a in pushThese)
         {
-            foreach (Collider a in pushThese)
-            {
-                Debug.Log("pushing this guy: " + a);
-                a.GetComponent<Rigidbody>().AddForce(-(player.transform.position - a.transform.position) * intensity*2);
-            }
+            force = Vector3.Normalize(player.transform.position - a.transform.position);
+            a.GetComponent<Rigidbody>().AddForce(force * intensity);
         }
+
     }
     void forcePull()
     {
         pullThese = Physics.OverlapBox(new Vector3(transform.position.x + 10, transform.position.y, transform.position.z),
-            new Vector3(15f, 2, 2), Quaternion.identity, layers);
-        if (pullThese.Length > 0)
+            new Vector3(10f, 2, 2), Quaternion.identity, layers);
+        foreach (Collider a in pullThese)
         {
-            foreach (Collider a in pullThese)
-            {
-                Debug.Log("pulling this guy: " + a);
-                a.GetComponent<Rigidbody>().AddForce((player.transform.position - a.transform.position) * intensity / 2);
-            }
+            force = Vector3.Normalize(player.transform.position - a.transform.position);
+            a.GetComponent<Rigidbody>().AddForce(-(force) * intensity);
         }
     }
+
 }
